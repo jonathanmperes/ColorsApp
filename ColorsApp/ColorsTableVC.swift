fc44d33e03ef43bae7c49759de7f0f6dd52adf54
jonathanmarceloperes <emailmarceloperes@gmail.com>
2023-07-11 22:35:55 -0300

    func createRandomColor and func addRandomColors created

--- ColorsApp/ColorsTableVC.swift
+++ ColorsApp/ColorsTableVC.swift
@@ -8,18 +8,33 @@
 import UIKit
 
 class ColorsTableVC: UIViewController {
+    
+    var colors: [UIColor] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addRandomColors()
+    }
+    
+    func addRandomColors() {
+        for _ in 0..<50 {
+            colors.append(createRandomColor())
+        }
+    }
+    
+    func createRandomColor() -> UIColor {
+        let randomColor = UIColor(red: CGFloat.random(in: 0...1),
+                                  green: CGFloat.random(in: 0...1),
+                                  blue: CGFloat.random(in: 0...1),
+                                  alpha: 1)
+        return randomColor
     }
 }
 
 extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return colors.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
